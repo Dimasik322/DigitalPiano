3e80bd3194151d60b0307c2ca600911083bd62ea
Dimasik322 <dimakonovalov88@gmail.com>
2024-11-22 23:40:00 +0400

    Добавьте файлы проекта.

---
+++ Digital Piano/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Digital_Piano {
+    public partial class MainWindow : Window {
+
+        private Piano piano;
+
+        public MainWindow() {
+            piano = new Piano();
+            InitializeComponent();
+        }
+
+        bool isNotesNamesVisible = true;
+        private bool isMenuVisible = false;
+
+        private void ToggleMenuButton_Click(object sender, RoutedEventArgs e) {
+            if (isMenuVisible) {
+                CloseMenu();
+            }
+            else {
+                OpenMenu();
+            }
+        }
+
+        private void OpenMenu() {
+            // Скрытие кнопки ToggleMenu
+            ToggleMenuButton.Visibility = Visibility.Collapsed;
+
+            // Показ меню и Overlay
+            Overlay.Visibility = Visibility.Visible;
+
+            // Запуск анимации для открытия меню
+            Storyboard showMenu = (Storyboard)FindResource("ShowMenuAnimation");
+            showMenu.Begin();
+
+            isMenuVisible = true;
+        }
+
+        private void CloseMenu() {
+            // Закрытие меню
+            Storyboard hideMenu = (Storyboard)FindResource("HideMenuAnimation");
+            hideMenu.Completed += (s, ev) =>
+            {
+                // Скрытие Overlay и кнопки ToggleMenu
+                Overlay.Visibility = Visibility.Collapsed;
+                ToggleMenuButton.Visibility = Visibility.Visible;
+            };
+            hideMenu.Begin();
+
+            isMenuVisible = false;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e) {
+            // Обработка сохранения данных
+            MessageBox.Show("Данные сохранены!");
+
+            // Закрыть меню после сохранения
+            CloseMenu();
+        }
+
+        private void KeyClick(object sender, RoutedEventArgs e) {
+            Button clickedButton = sender as Button;
+            if (clickedButton != null) {
+                int semitoneOffset = int.Parse(clickedButton.Tag.ToString());
+                if (piano != null) {
+                    piano.PlayTone(semitoneOffset, 1);
+                }
+            }
+        }
+
+        private void VolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            if (VolumeSlider != null) {
+                int newVolume = (int)VolumeSlider.Value;
+                piano.volume = newVolume;
+                if (VolumeLabel != null) {
+                    VolumeLabel.Text = $"Volume: {newVolume}%";
+                }
+            }
+        }
+    }
+}
---
+++ Digital Piano/Piano.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+
+namespace Digital_Piano {
+    public class Piano {
+        protected const int SampleRate = 44100;
+        protected double basefreq;
+        protected uint sustain;
+        protected uint reverb;
+        protected uint chorus;
+        public int volume;
+        protected int pitch;
+
+        public Piano() {
+            this.basefreq = 440.0;
+            this.sustain = 0;   
+            this.reverb = 0;
+            this.chorus = 0;
+            this.volume = 50;
+            this.pitch = 0;
+        }
+
+        private double GetNoteFrequency(int semitoneOffset) {
+            return basefreq * Math.Pow(2, semitoneOffset / 12.0);
+        }
+
+        public void PlayTone(int semitoneOffset, double durationSeconds) {
+            int samplesCount = (int)(SampleRate * durationSeconds);
+            var buffer = new float[samplesCount];
+
+            // Генерация синусоиды для нужной частоты
+            for (int i = 0; i < samplesCount; i++) {
+                buffer[i] = (float)Math.Sin(2 * Math.PI * GetNoteFrequency(semitoneOffset) * i / SampleRate);
+            }
+
+            // Используем WaveOutEvent для воспроизведения
+            using (var waveOut = new WaveOutEvent()) {
+                var waveProvider = new BufferedWaveProvider(WaveFormat.CreateIeeeFloatWaveFormat(SampleRate, 1));
+                waveProvider.AddSamples(GetByteArrayFromFloatArray(buffer), 0, buffer.Length * sizeof(float));
+                waveOut.Init(waveProvider);
+                waveOut.Play();
+                Thread.Sleep((int)(durationSeconds * 1000)); // Задержка для завершения воспроизведения
+            }
+        }
+        private byte[] GetByteArrayFromFloatArray(float[] buffer) {
+            var byteArray = new byte[buffer.Length * sizeof(float)];
+            Buffer.BlockCopy(buffer, 0, byteArray, 0, byteArray.Length);
+            return byteArray;
+        }
+    }
+
+}
