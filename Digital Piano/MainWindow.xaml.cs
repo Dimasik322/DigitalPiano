328f45a91d19bacd77dcd301bd68cec85e3e2ce3
Dimasik322 <dimakonovalov88@gmail.com>
2024-11-27 22:34:35 +0400

    0.1.8

--- Digital Piano/MainWindow.xaml.cs
+++ Digital Piano/MainWindow.xaml.cs
@@ -26,20 +26,71 @@ namespace Digital_Piano {
 
         private bool isNotesNamesVisible = true;
         private bool isMenuVisible = false;
+        private bool isExitMenuVisible = false;
 
         private void ToggleMenuButton_Click(object sender, RoutedEventArgs e) {
             ToggleMenuButton.Visibility = Visibility.Collapsed;
-            Overlay.Visibility = Visibility.Visible;
+            OverlayMenu.Visibility = Visibility.Visible;
             Storyboard showMenu = (Storyboard)FindResource("ShowMenuAnimation");
             showMenu.Begin();
             isMenuVisible = true;
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e) {
+            base.OnKeyDown(e);
+            if (e.Key == Key.Escape) {
+                if (isMenuVisible) {
+                    HideSideBarMenu();
+                    return;
+                }
+                if (!isExitMenuVisible) {
+                    ShowExitMenu();
+                    return;
+                }
+                HideExitMenu();
+            }
+        }
+
+        private void HideSideBarMenu() {
+            Storyboard hideMenu = (Storyboard)FindResource("HideMenuAnimation");
+            hideMenu.Completed += (s, ev) => {
+                OverlayMenu.Visibility = Visibility.Collapsed;
+                ToggleMenuButton.Visibility = Visibility.Visible;
+            };
+            hideMenu.Begin();
+            isMenuVisible = false;
+        }
+
+        private void ShowExitMenu() {
+            OverlayExit.Visibility = Visibility.Visible;
+            Storyboard showMenu = (Storyboard)FindResource("ShowExitMenuAnimation");
+            showMenu.Begin();
+            isExitMenuVisible = true;
+        }
+
+        private void HideExitMenu() {
+            Storyboard hideMenu = (Storyboard)FindResource("HideExitMenuAnimation");
+            hideMenu.Completed += (s, e) =>
+            {
+                OverlayExit.Visibility = Visibility.Collapsed;
+            };
+            hideMenu.Begin();
+            isExitMenuVisible = false;
+        }
+
+        private void YesButton_Click(object sender, RoutedEventArgs e) {
+            Application.Current.Shutdown();
+        }
+
+        private void NoButton_Click(object sender, RoutedEventArgs e) {
+            HideExitMenu();
         }
 
         private void SaveButton_Click(object sender, RoutedEventArgs e) {
             MessageBox.Show("Данные сохранены!");
             Storyboard hideMenu = (Storyboard)FindResource("HideMenuAnimation");
             hideMenu.Completed += (s, ev) => {
-                Overlay.Visibility = Visibility.Collapsed;
+                OverlayMenu.Visibility = Visibility.Collapsed;
                 ToggleMenuButton.Visibility = Visibility.Visible;
             };
             hideMenu.Begin();
